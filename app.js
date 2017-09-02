94042da6d759074e62586b8c5fd77f4e169591b5
José Castro <jcastro@solcomp.com>
2017-09-01 21:59:25 -0600

    Creacion servidor estatico y recurso hola mundo.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-'use strict';
+const { express, server } = require('ful-ms-svr');
+let port = 3000;
+console.log(`Hello world from scrumSrv. ${process.pid}`);
+server.port = port;
+server.run(port);
 
-console.log('Hello world, this is the Bootcamp SCRUM Server on Node.js');
+let router = express.Router();
+server.use(express.static('public'));
+server.use('/api', router);
+
+router.get('/test', (req, res) => {
+    res.send('Hello world');
+});
