97f8642b1c0c9565fdc923bcee047e18521bfef7
José Castro <jcastro@solcomp.com>
2017-09-01 23:14:47 -0600

    Se agrega conexion a db

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const { express, server } = require('ful-ms-svr');
 let port = 3000;
 console.log(`Hello world from scrumSrv. ${process.pid}`);
 server.port = port;
+server.use(require('body-parser').json());
 server.run(port);
 
 let router = express.Router();
+let dbSettings = require('./dbSettings');
 server.use(express.static('public'));
 server.use('/api', router);
-
-router.get('/test', (req, res) => {
-    res.send('Hello world');
-});
+require('./controllers/priority')(router);
+let db = require('knex')(dbSettings.dbScrumSettings);
+console.log(db);
---
+++ controllers/priority.js
@@ -0,0 +1,21 @@
+﻿module.exports = function (router, db) {
+    router.get('/priorities', (req, res) => {
+        res.json({ test: 'all' });
+    });
+
+    router.get('/priorities/:id', (req, res) => {
+        res.json({ test: 'get by id.' });
+    });
+
+    router.post('/priorities', (req, res) => {
+        res.json({ test: 'post' });
+    });
+
+    router.delete('/priorities/:id', (req, res) => {
+        res.json({ test: 'delete' });
+    });
+
+    router.put('/priorities/:id', (req, res) => {
+        res.json({ test: 'put' });
+    })
+};
