3b9ca03c978e02bafe870c5261cf75f3e3e890bf
José Luis Castro <joseluiscastro23@outlook.com>
2017-09-19 20:44:02 -0600

    Unit Test implementation phase 1

--- dbSettings.js
+++ dbSettings.js
@@ -1,10 +1,33 @@
-﻿exports.dbScrumSettings = {
-    client: 'mssql',
-    connection: {
-        host: 'localhost\\MSSQLSERVER',
-        user: 'sa',
-        password: '12345678',
-        database: 'scrumdb',
-        port: 1433
+﻿//exports.dbScrumSettings = {
+//    client: 'mssql',
+//    connection: {
+//        host: 'localhost\\MSSQLSERVER',
+//        user: 'sa',
+//        password: '12345678',
+//        database: 'scrumdb',
+//        port: 1433
+//    }
+//};
+
+module.exports = {
+    development: {
+        client: 'mssql',
+        connection: {
+            host: 'localhost\\MSSQLSERVER',
+            user: 'sa',
+            password: '12345678',
+            database: 'scrumdb',
+            port: 1433
+        }
+    },
+    test: {
+        client: 'mssql',
+        connection: {
+            host: 'localhost\\MSSQLSERVER',
+            user: 'sa',
+            password: '12345678',
+            database: 'scrumdbtest',
+            port: 1433
+        }
     }
-};
+}
