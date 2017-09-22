e5b61b4d338ad5af60310a88f7a003a7c8ad5a4d
José Luis Castro <joseluiscastro23@outlook.com>
2017-09-21 21:02:52 -0600

    Features-state resource

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -7,5 +7,6 @@ router.use('/api', require('./features'));
 router.use('/api', require('./sprints'));
 router.use('/api', require('./stories'));
 router.use('/api', require('./tasks'));
+router.use('/api', require('./features_state'));
 
 module.exports = router;
---
+++ models/features_state.js
@@ -0,0 +1,18 @@
+﻿const table = 'tbl_feature_state';
+const allFields = ['feature_state_id as id', 'feature_state_nm as name'];
+const idField = 'feature_state_id';
+const db = require('./dbConnection');
+
+module.exports = {
+    getAll: (fields) => {
+
+        console.assert(!fields || fields instanceof Array, 'Fields must be an Array.');
+        return db.select(fields || allFields).from(table);
+    },
+
+    getById: (id, fields) => {
+
+        console.assert(!fields || fields instanceof Array, 'Fields must be an Array.');
+        return db.select(fields || allFields).from(table).where(idField, id);
+    }
+};
